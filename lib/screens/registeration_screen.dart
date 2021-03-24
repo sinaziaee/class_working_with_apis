7287444f8340d0443166187bc18704bcf5e5e8e7
sinaziaee <sinaziaee99@gmail.com>
2021-03-25 00:48:02 +0430

    First Commit

---
+++ lib/screens/registeration_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterationScreen extends StatefulWidget {
+  @override
+  _RegisterationScreenState createState() => _RegisterationScreenState();
+}
+
+class _RegisterationScreenState extends State<RegisterationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
